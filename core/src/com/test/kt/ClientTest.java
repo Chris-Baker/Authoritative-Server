e0660e61d5bb40faa5e849a5ddf1728bdf523fcc
Chris Baker <chris.baker@semantico.com>
2017-05-11 14:04:08 +0100

    Render a cube as the player object

--- core/src/com/test/kt/ClientTest.java
+++ core/src/com/test/kt/ClientTest.java
@@ -3,6 +3,10 @@ package com.test.kt;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Client;
@@ -16,15 +20,26 @@ import java.io.IOException;
 
 public class ClientTest extends ApplicationAdapter {
 
+	// network
     private Client client;
+
+    // simulation
     private Simulation simulation;
+
+    // player movement
     private boolean moveLeft = false;
     private boolean moveRight = false;
     private boolean jump = false;
 
+    // render
+	ShapeRenderer shapeRenderer;
+	Camera camera;
+
 	@Override
 	public void create () {
 
+		camera = new OrthographicCamera(640, 480);
+		shapeRenderer = new ShapeRenderer();
         simulation = new Simulation();
 
 		try {
@@ -63,10 +78,12 @@ public class ClientTest extends ApplicationAdapter {
 	@Override
 	public void render () {
 
+		// get user input
         this.moveLeft = Gdx.input.isKeyPressed(Keys.A);
         this.moveRight = Gdx.input.isKeyPressed(Keys.D);
         this.jump = Gdx.input.isKeyPressed(Keys.SPACE);
 
+        // create network request
         CharacterControllerMessage request = new CharacterControllerMessage();
         request.timestamp = TimeUtils.nanoTime();
         request.moveLeft = this.moveLeft;
@@ -75,9 +92,18 @@ public class ClientTest extends ApplicationAdapter {
 
         client.sendUDP(request);
 
-        System.out.println(simulation.px + ", " + simulation.py);
+
+        // render the world
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		camera.update();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		shapeRenderer.setColor(1, 1, 0, 1);
+		shapeRenderer.rect(simulation.px, simulation.py, 32, 32);
+		shapeRenderer.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 
