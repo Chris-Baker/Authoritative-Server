083f8e37b2ef206193c0d064a25a277ddc20e667
Chris Baker <chris.baker@semantico.com>
2017-05-12 14:46:58 +0100

    Add simulation snapshot object so we can adjust the client's simulation with authoritative data form the server

---
+++ core/src/com/test/kt/SimulationSnapshot.java
@@ -0,0 +1,10 @@
+package com.test.kt;
+
+/**
+ * Created by chrisb on 10/05/2017.
+ */
+public class SimulationSnapshot {
+    public float timestamp;
+    public float px = 0;
+    public float py = 0;
+}
